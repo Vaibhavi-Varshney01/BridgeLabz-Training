d268b6ba7902a1d7d2fa4416b6746151c01e2ca0
Vaibhavi01 <varshneyvaibhavi65@gmail.com>
2026-01-08 19:52:19 +0530

    Removed unwanted folders

--- core-csharp-practise-folder/gcr-codebase/csharp-programming-elements/level-1/MaxHandshakes.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class MaxHandshakes{
-  public static void Main(String [] args){
-    Console.WriteLine("Enter the number of Students: ");
-    int N = int.Parse(Console.ReadLine());
-    int combi = (N*(N-1))/2;
-    Console.WriteLine("Possible no. of handshakes is : " + combi);
-  }
-}
